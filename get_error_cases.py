de84e4af357be492f65ce56c811a53ff69df516e
Qiusi Zhan <qiusi@rgpu02.ncsa.illinois.edu>
2023-11-03 13:58:29 -0500

    non_executable

--- get_error_cases.py
+++ get_error_cases.py
@@ -17,9 +17,25 @@ def error_without_SQL():
                 item['input_prompt'] = prompt
                 f.write(json.dumps(item) + '\n')
 def error_non_executable():
-    ececution_results = os.listdir('./execution__results/')
+    execution_results = [file.split('.')[0] for file in os.listdir('./execution_results/')]
+    print(execution_results)
+    # assert 1==0
+    non_executable_case_ids = []
     for file in os.listdir('./query_results/'):
         query_id = file.split('.')[0]
+        if query_id not in execution_results:
+            non_executable_case_ids.append(query_id)
+    print(len(non_executable_case_ids))
+    non_executable_cases = []
+    with open('predicted_results.jsonl', 'r') as f:
+        for line in f.readlines():
+            tmp = json.loads(line)
+            if str(tmp['id']) in non_executable_case_ids:
+                non_executable_cases.append(line)
+    with open("./error_cases/non_executable.jsonl", 'w') as f:
+        for case in non_executable_cases:
+            f.write(case)
         
 if __name__=='__main__':
+    # error_without_SQL()
     error_non_executable()
